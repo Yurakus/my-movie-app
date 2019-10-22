fc0b19b642086bae59258c3164e03e3c3f546847
Juraj Paluch <juraj.paluch@gmail.com>
2019-10-22 11:42:00 +0200

    Updates

---
+++ precache-manifest.9174fcf3d49a8a368f8aadcb103e31e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de6d7e9189b98e1d7b9576f64e34ca49",
+    "url": "/my-movie-app/index.html"
+  },
+  {
+    "revision": "105b4b6d8debc0c1b2dd",
+    "url": "/my-movie-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3538911ffb35b8724853",
+    "url": "/my-movie-app/static/js/2.ace72074.chunk.js"
+  },
+  {
+    "revision": "105b4b6d8debc0c1b2dd",
+    "url": "/my-movie-app/static/js/main.a093a2ab.chunk.js"
+  },
+  {
+    "revision": "6cfbfec8537224592eb5",
+    "url": "/my-movie-app/static/js/runtime-main.71b5f792.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-movie-app/static/media/logo.25bf045c.svg"
+  }
+]);
